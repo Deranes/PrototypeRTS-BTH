5413e7ec2a4ee39e281751cb3ce6d8e4875e19e6
Ola Enberg <deranes21@hotmail.com>
2014-11-12 09:41:46 +0100

    The player can now also attack enemy units by right clicking one of them

--- src/game/entities/HumanPlayer.cpp
+++ src/game/entities/HumanPlayer.cpp
@@ -2,13 +2,32 @@
 
 #include "../../input/GlobalMouse.h"
 #include "Unit.h"
+#include "../../math/Math.h"
+
+using namespace sf;
 
 void HumanPlayer::Update( std::vector<Unit>& unitsFriendly, std::vector<Unit>& unitsHostile ) {
 	if ( g_Mouse.RightClick() ) {
 		Vector2f targetPosition = Vector2f( g_Mouse.Position().x, g_Mouse.Position().y );
+		Unit* enemyTarget = nullptr;
 
-		for ( auto& unit : unitsFriendly ) {
-			unit.CommandMove( targetPosition );
+		// Check if player clicked an enemy
+		for ( auto& unit : unitsHostile ) {
+			const Vector2f distance = unit.GetPosition() - targetPosition;
+
+			if ( math::vec2f::LengthSqrd( distance ) <= UNIT_RADIUS * UNIT_RADIUS ) {
+				enemyTarget = &unit;
+			}
+		}
+
+		if ( enemyTarget ) {
+			for ( auto& unit : unitsFriendly ) {
+				unit.CommandAttackTarget( enemyTarget );
+			}
+		} else {
+			for ( auto& unit : unitsFriendly ) {
+				unit.CommandMove( targetPosition );
+			}
 		}
 	}
 }
